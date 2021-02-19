9b637b3602d6fead85de1ff85c181efd20e26a3d
steffno <49401703+steffno@users.noreply.github.com>
2021-02-19 14:38:53 +0100

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,12 +3,12 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = ' Stefano Di Dionisio';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
-const gree2 = 'Good morning!  ';
-const gree3 = 'Good afternoon  ';
+const gree1 = 'Buonanotte!  ';
+const gree2 = 'Buongiorno!  ';
+const gree3 = 'Buon pomeriggio..  ';
 const gree4 = 'Good evening,  ';
 const gree5 = 'Good evening,  ';
 const gree6 = 'Good evening,  ';
